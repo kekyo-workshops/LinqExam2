a263beedc5089d37399b293d54b1132a6d66664c
Kouji Matsui <k@kekyo.net>
2016-09-27 16:08:13 +0900

    Initial commit.

---
+++ LinqExam2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExam2
+{
+    class Program
+    {
+        private static string[] ExtractByNumberByLinq(TextFieldContext data, int floor)
+        {
+            // 01101,"064  ","0640941","ホッカイドウ","サッポロシチュウオウク","アサヒガオカ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0
+            return
+                data.
+                Where(columns => int.Parse(columns[2]) >= floor).  // 900-0000
+                OrderBy(columns => columns[2]).
+                Select(columns => $"{columns[2]}, {columns[6]}{columns[7]}{columns[8]}").
+                ToArray();
+        }
+
+        private static string[] ExtractByNumberByLinqQuery(TextFieldContext data, int floor)
+        {
+            // 01101,"064  ","0640941","ホッカイドウ","サッポロシチュウオウク","アサヒガオカ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0
+            return
+               (from columns in data
+                where int.Parse(columns[2]) >= floor
+                orderby columns[2]
+                select $"{columns[2]}, {columns[6]}{columns[7]}{columns[8]}").
+                ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var x_ken_all = new TextFieldContext("x_ken_all.csv");
+
+            //var results = ExtractByNumberByLinq(x_ken_all, 9000000);
+            var results = ExtractByNumberByLinqQuery(x_ken_all, 9000000);
+            Console.WriteLine(string.Join("\r\n", results));
+        }
+    }
+}
---
+++ LinqExam2/TextFieldContext.cs
@@ -0,0 +1,86 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TextFieldContext
+// Copyright (c) 2016 Kouji Matsui (@kekyo2)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+
+namespace LinqExam2
+{
+    public sealed class TextFieldContext : IEnumerable<string[]>
+    {
+        private readonly string path_;
+        private readonly string separator_;
+        private readonly Encoding encoding_;
+
+        public TextFieldContext(string path, string separator = ",", Encoding encoding = null)
+        {
+            path_ = path;
+            separator_ = separator;
+            encoding_ = encoding;
+
+            this.FieldNames = new string[0];
+        }
+
+        public string[] FieldNames
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerator<string[]> GetEnumerator()
+        {
+            try
+            {
+                using (var stream = new FileStream(path_, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using (var parser = new TextFieldParser(stream, encoding_ ?? Encoding.UTF8, true, false))
+                    {
+                        parser.TextFieldType = FieldType.Delimited;
+                        parser.Delimiters = new[] { separator_ };
+                        parser.HasFieldsEnclosedInQuotes = true;
+                        parser.TrimWhiteSpace = true;
+
+                        if (parser.EndOfData == false)
+                        {
+                            this.FieldNames = parser.ReadFields();
+
+                            while (parser.EndOfData == false)
+                            {
+                                var fields = parser.ReadFields();
+                                yield return fields;
+                            }
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                this.FieldNames = new string[0];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
