1ecf97945de8eb3916c59a6e07ca99baaf05712e
Kouji Matsui <k@kekyo.net>
2016-09-27 19:19:23 +0900

    Add TestJoin.

--- LinqExam2/Program.cs
+++ LinqExam2/Program.cs
@@ -8,6 +8,7 @@ namespace LinqExam2
 {
     class Program
     {
+        #region Samples
         private static string[] ExtractByNumberByLinq(TextFieldContext data, int floor)
         {
             // 01101,"064  ","0640941","ホッカイドウ","サッポロシチュウオウク","アサヒガオカ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0
@@ -30,13 +31,35 @@ namespace LinqExam2
                 ToArray();
         }
 
-        static void Main(string[] args)
+        private static void TestSample()
         {
             var x_ken_all = new TextFieldContext("x_ken_all.csv");
 
             //var results = ExtractByNumberByLinq(x_ken_all, 9000000);
             var results = ExtractByNumberByLinqQuery(x_ken_all, 9000000);
             Console.WriteLine(string.Join("\r\n", results));
+        }
+        #endregion
+
+        private static void TestJoin()
+        {
+            var x_ken_all = new TextFieldContext("x_ken_all.csv");
+
+            // 01101,"064  ","0640941","ホッカイドウ","サッポロシチュウオウク","アサヒガオカ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0
+            var ids = new[] {8102, 50302, 62924, 72962};
+            var results =
+                from columns in x_ken_all
+                join id in ids
+                    on int.Parse(columns[1]) equals id
+                select $"{columns[2]}, {columns[6]}{columns[7]}{columns[8]}";
+
+            Console.WriteLine(string.Join("\r\n", results));
+        }
+
+        static void Main(string[] args)
+        {
+            //TestSample();
+            TestJoin();
         }
     }
 }
